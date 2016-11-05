c23571952f63d6355dd92040741d3ca80da36431
ctrador <chad.trador@interapthq.com>
2016-11-05 09:30:43 -0400

    Initial Commit

---
+++ PeopleMon2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeopleMon2
+//
+//  Created by Chad Trador on 11/5/16.
+//  Copyright © 2016 Chad Trador. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
